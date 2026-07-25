103204c171ba4ced897cefdd9da0d2b5de604de3
chocotea1008 <chocotea1008@users.noreply.github.com>
2026-07-26 00:28:41 +0900

    Clean temp test files

--- inspect_solvewordle.py
+++
@@ -1,8 +0,0 @@
-import re, json
-
-with open('solver.js', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-# Inspect solveWordle function in solver.js
-pos = text.find('function solveWordle')
-print(text[pos:pos+1500])
